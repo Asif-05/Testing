129340d33bd25cb9ea6df42c3a25ed97565bb55d
DELL <DELL@DELL-PC>
2022-11-02 13:10:07 +0530

    Batch 7385 demo

---
+++ src/training/Register.java
@@ -0,0 +1,41 @@
+package training;
+
+public class Register {
+	
+		
+	public static int add(int a, int b)
+	{
+		int c=a+b;
+		System.out.println("The addition value is : "+c);
+		return c;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		add(3,2);
+		add(5,4);
+		add(7,5);
+		add(9,2);
+		add(7,4);
+		add(3,2);
+		add(5,4);
+		add(7,5);
+		add(9,2);
+		add(7,4);
+		add(3,2);
+		add(5,4);
+		add(7,5);
+		add(9,2);
+		add(7,4);
+		add(3,2);
+		add(5,4);
+		add(7,5);
+		add(9,2);
+		add(7,4);
+				
+		
+	}
+
+}
